af8ac40d0ff01845ac8488e4173eb948cd379008
moonrailgun <moonrailgun@gmail.com>
2015-06-19 22:05:16 +0800

    数据库操作内核
    ADD 数据库操作内核

---
+++ core/db.php
@@ -0,0 +1,33 @@
+﻿<?php
+class db{
+	function GetCon()
+	{
+		$con = mysql_connect($db_servername,$db_username,$db_password);
+		if(!$con)
+		{
+			die('Could not connect: ' . mysql_error());
+		}
+		return $con;
+	}
+	
+	function CloseCon($con)
+	{
+		mysql_close($con);
+	}
+	
+	function Query($dbname,$command)
+	{
+		$con = GetCon();
+		mysql_select_db("my_db", $con);
+	
+		$result = mysql_query($command);
+		return $result;
+	}
+	
+	$db_servername = "localhost";
+	$db_username = "root";
+	$db_password = "";
+}
+
+
+?>
