780eb4acd6abeb6152210b47ab9ba22288a5af0a
moonrailgun <moonrailgun@gmail.com>
2015-06-21 14:45:26 +0800

    JSON模型与封装
    ADD 英雄查询数据模型
    ADD JSON封装

---
+++ core/model.php
@@ -0,0 +1,16 @@
+﻿<?php
+class heroInfo
+{
+	public $CardId;
+	public $CardName;
+	public $CardRarity;
+	public $BaseHealth;
+	public $GrowHealth;
+	public $BaseEnergy;
+	public $GrowEnergy;
+	public $BaseAttack;
+	public $GrowAttack;
+	public $BaseSpeed;
+	public $GrowSpeed;
+}
+?>
--- query/HeroInfo.php
+++ query/HeroInfo.php
@@ -1,28 +1,30 @@
 ﻿<?php
 require '../core/db.php';
+require '../core/model.php';
 $command = "SELECT * FROM card";
 
 $result = $core_db->Query($command);
-
+$heroInfoList = array();
 while($row = mysql_fetch_array($result))
 {
-	$cardid = $row['CardId'];
-	$cardname = $row['CardName'];
-	$cardrarity = $row['CardRarity'];
-	$basehealth = $row['BaseHealth'];
-	$growhealth = $row['GrowHealth'];
-	$baseenergy = $row['BaseEnergy'];
-	$growenergy = $row['GrowEnergy'];
-	$baseattack = $row['BaseAttack'];
-	$growattack = $row['GrowAttack'];
-	$basespeed = $row['BaseSpeed'];
-	$growspeed = $row['GrowSpeed'];
+	$heroInfo = new heroInfo;
+
+	$heroInfo->CardId = (int)$row['CardId'];
+	$heroInfo->CardName = $row['CardName'];
+	$heroInfo->CardRarity = (int)$row['CardRarity'];
+	$heroInfo->BaseHealth = (int)$row['BaseHealth'];
+	$heroInfo->GrowHealth = (int)$row['GrowHealth'];
+	$heroInfo->BaseEnergy = (int)$row['BaseEnergy'];
+	$heroInfo->GrowEnergy = (int)$row['GrowEnergy'];
+	$heroInfo->BaseAttack = (int)$row['BaseAttack'];
+	$heroInfo->GrowAttack = (int)$row['GrowAttack'];
+	$heroInfo->BaseSpeed = (int)$row['BaseSpeed'];
+	$heroInfo->GrowSpeed = (int)$row['GrowSpeed'];
 	
-	echo $cardid." ".$cardname." ".$cardrarity." ";
-	echo $basehealth." ".$growhealth." ";
-	echo $baseenergy." ".$growenergy." ";
-	echo $baseattack." ".$growattack." ";
-	echo $basespeed." ".$growspeed;
-	echo "<br />";
+	$heroInfoList[count($heroInfoList)] = $heroInfo;
 }
+$returnJson["Type"] = "hero";
+$returnJson["Count"] = count($heroInfoList);
+$returnJson["List"] = $heroInfoList;
+echo json_encode($returnJson);
 ?>
